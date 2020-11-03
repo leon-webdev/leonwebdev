50d34250e5f8249a7810afd8ed61c9caf29a0057
León G. Martínez Ostos <leonma19@gmail.com>
2020-11-02 23:42:08 -0500

    add yaml files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,41 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
-module.exports = {
-  /* Your site config here */
-  plugins: [],
+module.exports = ({
+  basePath = "/",
+  contentPath = "content/",
+  showThemeLogo = true,
+  theme = "dark-orange",
+}) => {
+  return {
+    siteMetadata: {
+      description: "Personal page of León G. Martínez Ostos",
+      locale: "en",
+      showThemeLogo,
+      title: "León G. Martínez Ostos - My Website",
+      author: `@leon_webdev`,
+    },
+    plugins: [
+      {
+        resolve: `gatsby-plugin-postcss`,
+        options: {
+          postCssPlugins: [
+            require("tailwindcss")(require("./tailwind.config")(theme)),
+            require("postcss-input-range"),
+            require("autoprefixer"),
+          ],
+        },
+      },
+      `gatsby-plugin-react-helmet`,
+      `gatsby-transformer-yaml`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: contentPath,
+        },
+      },
+      {
+        resolve: "gatsby-plugin-react-svg",
+      },
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+    ],
+  }
 }
